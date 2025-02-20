e0d247aeaf56aeee5cc45684ac7089ca17c1637d
danielo515 <rdanielo@gmail.com>
2025-02-20 14:26:14 +0100

    formatter

--- flake.nix
+++ flake.nix
@@ -11,46 +11,56 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-    let
-      username = "danielo";
-      useremail = "rdanielo@gmail.com";
-      system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
-      hostname = "Danielos-MacBook-Pro";
-      specialArgs = inputs // { inherit username useremail hostname system; };
-      configuration = { pkgs, system, ... }: {
-        # # Conflicts with determinate systems installation
-        # nix.enable = false;
-        # # Necessary for using flakes on this system.
-        # nix.settings.experimental-features = "nix-command flakes";
-        # # Set Git commit hash for darwin-version.
-        # system.configurationRevision = self.rev or self.dirtyRev or null;
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 6;
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-        # nixpkgs.hostPlatform = builtins.trace "System: ${system}" system;
-      };
-    in {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#Danielos-MacBook-Pro
-      darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
-        inherit system specialArgs;
-        modules = [
-          ./modules/nix-core.nix
-          ./modules/system.nix
-          ./modules/apps.nix
-          ./modules/host-users.nix
-          configuration
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = specialArgs;
-            home-manager.users.${username} = import ./home;
-          }
-        ];
-      };
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+  }: let
+    username = "danielo";
+    useremail = "rdanielo@gmail.com";
+    system = "aarch64-darwin"; # aarch64-darwin or x86_64-darwin
+    hostname = "Danielos-MacBook-Pro";
+    specialArgs = inputs // {inherit username useremail hostname system;};
+    configuration = {
+      pkgs,
+      system,
+      ...
+    }: {
+      # # Conflicts with determinate systems installation
+      # nix.enable = false;
+      # # Necessary for using flakes on this system.
+      # nix.settings.experimental-features = "nix-command flakes";
+      # # Set Git commit hash for darwin-version.
+      # system.configurationRevision = self.rev or self.dirtyRev or null;
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      # nixpkgs.hostPlatform = builtins.trace "System: ${system}" system;
     };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Danielos-MacBook-Pro
+    darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
+      inherit system specialArgs;
+      modules = [
+        ./modules/nix-core.nix
+        ./modules/system.nix
+        ./modules/apps.nix
+        ./modules/host-users.nix
+        configuration
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users.${username} = import ./home;
+        }
+      ];
+    };
+    # nix code formatter
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+  };
 }
