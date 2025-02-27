f4d703baab83fbd391dadd08962501b31906b455
danielo <rdanielo@gmail.com>
2025-02-27 22:40:19 +0100

    chore: remove checks

--- flake.nix
+++ flake.nix
@@ -20,65 +20,30 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-    nix-darwin,
-    home-manager,
-    ...
-  }: let
-    # Define common variables
-    username = "danielo";
-    useremail = "rdanielo@gmail.com";
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux"];
+  outputs =
+    inputs@{ self, nixpkgs, flake-parts, nix-darwin, home-manager, ... }:
+    let
+      # Define common variables
+      username = "danielo";
+      useremail = "rdanielo@gmail.com";
+    in flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ];
 
-      perSystem = {
-        system,
-        pkgs,
-        lib,
-        ...
-      }: {
+      perSystem = { system, pkgs, lib, ... }: {
         # Formatter for each system
         formatter = pkgs.alejandra;
 
         # Development shells
-        devShells.default = pkgs.mkShell {buildInputs = [pkgs.nixd];};
+        devShells.default = pkgs.mkShell { buildInputs = [ pkgs.nixd ]; };
 
-        # Add checks to verify that configurations can be evaluated
-        checks = let
-          # Helper function to check if an attribute set is non-empty
-          hasConfigs = configs:
-            builtins.length (builtins.attrNames configs) > 0;
-
-          # Check that configurations exist
-          darwinConfigsExist = hasConfigs self.darwinConfigurations;
-          homeConfigsExist = hasConfigs self.homeConfigurations;
-
-          # Throw errors if configurations don't exist
-          _ =
-            lib.trivial.throwIfNot darwinConfigsExist
-            "Darwin configurations are missing";
-          __ =
-            lib.trivial.throwIfNot homeConfigsExist
-            "Home configurations are missing";
-        in {
-          # Simple check that verifies configurations exist and can be evaluated
-          config-check = pkgs.runCommand "configuration-check" {} ''
-            # If we got here, all the assertions in the let binding passed
-            mkdir -p $out
-            echo "All configurations can be evaluated!" > $out/result
-          '';
-        };
       };
 
       flake = {
         # Darwin configurations
         darwinConfigurations = let
           # Common special arguments for all configurations
-          commonSpecialArgs = {inherit username useremail;};
+          commonSpecialArgs = { inherit username useremail; };
 
           # Host configurations
           darwinHosts = {
@@ -97,13 +62,11 @@
           mkDarwinConfig = hostname: hostConfig:
             nix-darwin.lib.darwinSystem {
               system = hostConfig.system;
-              specialArgs =
-                commonSpecialArgs
-                // {
-                  hostname = hostname;
-                  system = hostConfig.system;
-                  isDarwin = true;
-                };
+              specialArgs = commonSpecialArgs // {
+                hostname = hostname;
+                system = hostConfig.system;
+                isDarwin = true;
+              };
               modules = [
                 # Host-specific configuration
                 hostConfig.hostPath
@@ -114,33 +77,30 @@
                   # Home Manager configuration
                   home-manager.useGlobalPkgs = true;
                   home-manager.useUserPackages = true;
-                  home-manager.extraSpecialArgs =
-                    commonSpecialArgs
-                    // {
-                      isDarwin = true;
-                    };
+                  home-manager.extraSpecialArgs = commonSpecialArgs // {
+                    isDarwin = true;
+                  };
                   home-manager.users.${username} = import ./home;
                   home-manager.backupFileExtension = "home-bk";
                 }
 
                 # Include overlays
-                {nixpkgs.overlays = import ./overlays;}
+                { nixpkgs.overlays = import ./overlays; }
               ];
             };
-        in
-          builtins.mapAttrs mkDarwinConfig darwinHosts;
+        in builtins.mapAttrs mkDarwinConfig darwinHosts;
 
         # Standalone home-manager configurations
         homeConfigurations = let
           # Common special arguments for all configurations
-          commonSpecialArgs = {inherit username useremail;};
+          commonSpecialArgs = { inherit username useremail; };
 
           # Function to create standalone home-manager configuration
           mkHomeConfig = system: isDarwin:
             home-manager.lib.homeManagerConfiguration {
               pkgs = nixpkgs.legacyPackages.${system};
-              modules = [./home];
-              extraSpecialArgs = commonSpecialArgs // {inherit isDarwin;};
+              modules = [ ./home ];
+              extraSpecialArgs = commonSpecialArgs // { inherit isDarwin; };
             };
         in {
           # Darwin standalone home configuration
