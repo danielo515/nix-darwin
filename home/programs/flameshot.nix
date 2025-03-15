de6f3ef9d60e94803e6e2540f703498cc8ae86ca
danielo515 <rdanielo@gmail.com>
2025-03-15 09:25:02 +0100

    flameshot uses toINI instead of string concat

--- home/programs/flameshot.nix
+++ home/programs/flameshot.nix
@@ -1,35 +1,33 @@
 # flameshot is a free,open source and cross platform screenshot manager
-{
-  pkgs,
-  username,
-  ...
-}:
+{ pkgs, username, ... }:
 let
-  savePath = "${if pkgs.stdenv.isDarwin then "/Users" else "/home"}/${username}/screenshots";
-in
-{
+  savePath = "${
+      if pkgs.stdenv.isDarwin then "/Users" else "/home"
+    }/${username}/screenshots";
+in {
   # Ensure flameshot package installed
-  home.packages = with pkgs; [
-    flameshot
-  ];
+  home.packages = with pkgs; [ flameshot ];
 
   xdg.configFile = {
-    "flameshot/flameshot.ini".text = ''
-      [General]
-      disabledTrayIcon=true
-      contrastOpacity=96
-      drawColor=#ff2800
-      drawFontSize=4
-      saveAsFileExtension=.png
-      savePath = ${savePath};
-      savePathFixed=true
-      showDesktopNotification=false
-      showHelp=false
-      showMagnifier=true
-      showStartupLaunchMessage=false
-      squareMagnifier=true
-      startupLaunch=true
-      uiColor=#8aadf4
-    '';
+    "flameshot/flameshot.ini" = {
+      text = pkgs.lib.generators.toINI { } {
+        General = {
+          disabledTrayIcon = true;
+          contrastOpacity = 96;
+          drawColor = "#ff2800";
+          drawFontSize = 4;
+          saveAsFileExtension = ".png";
+          savePath = savePath;
+          savePathFixed = true;
+          showDesktopNotification = false;
+          showHelp = false;
+          showMagnifier = true;
+          showStartupLaunchMessage = false;
+          squareMagnifier = true;
+          startupLaunch = true;
+          uiColor = "#8aadf4";
+        };
+      };
+    };
   };
 }
