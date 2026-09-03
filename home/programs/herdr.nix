d19fc854fb197998a10d1e4f3a988178ff87a54a
danielo515 <rdanielo@gmail.com>
2026-09-03 08:47:45 +0200

    fix(herdr): copy jump script into store via relative path (OS-agnostic)

--- home/programs/herdr.nix
+++ home/programs/herdr.nix
@@ -23,9 +23,10 @@ in {
     xdg.configFile."herdr/config.toml".source =
       config.lib.file.mkOutOfStoreSymlink
       /etc/nix-darwin/dotfiles/herdr.toml;
-    xdg.configFile."herdr/jump.sh".source =
-      config.lib.file.mkOutOfStoreSymlink
-      /etc/nix-darwin/dotfiles/herdr-jump.sh;
+    xdg.configFile."herdr/jump.sh" = {
+      source = ../../dotfiles/herdr-jump.sh;
+      executable = true;
+    };
     xdg.configFile."herdr/plugins/config/persiyanov.reviewr/config.toml".source =
       config.lib.file.mkOutOfStoreSymlink
       /etc/nix-darwin/dotfiles/herdr-reviewr.toml;
