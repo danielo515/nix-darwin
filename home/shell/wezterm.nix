5bafeb563f95f442fca9a9c26c6285304102b964
danielo515 <rdanielo@gmail.com>
2026-09-05 20:59:56 +0200

    feat(wezterm): match cmux/ghostty look (catppuccin mocha, nerd font, blur)

--- home/shell/wezterm.nix
+++ home/shell/wezterm.nix
@@ -7,9 +7,19 @@ let
       config = wezterm.config_builder()
     end
 
-    config.color_scheme = 'Gruvbox Dark (Gogh)'
+    config.color_scheme = 'Catppuccin Mocha'
     config.enable_tab_bar = true
-    config.font = wezterm.font 'JetBrains Mono'
+    config.use_fancy_tab_bar = false
+    config.hide_tab_bar_if_only_one_tab = true
+    config.font = wezterm.font_with_fallback { 'JetBrainsMono Nerd Font', 'JetBrains Mono' }
+    config.font_size = 12
+    config.harfbuzz_features = { 'calt', 'liga', 'dlig' }
+    config.window_background_opacity = 0.85
+    config.macos_window_background_blur = 20
+    config.window_decorations = 'RESIZE'
+    config.default_cursor_style = 'BlinkingBlock'
+    config.hide_mouse_cursor_when_typing = true
+    config.send_composed_key_when_left_alt_is_pressed = false
     -- config.term = 'wezterm'
     config.window_padding = { left = 2, right = 2, top = 2, bottom = 2, }
     config.animation_fps = 1
